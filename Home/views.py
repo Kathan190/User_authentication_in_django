1559d8a6e4cc302d1af483c6d47c24c559343813
iamkathan <shethkathan4@gmail.com>
2023-10-04 05:24:58 +0100

    contact database has been created for contact user data

--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from Home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -11,5 +13,12 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        comment = request.POST.get("comment")
+        contact = Contact(name=name, email=email, phone=phone, comment=comment, date=datetime.today()) 
+        contact.save()
     return render(request, 'contact.html')
     
