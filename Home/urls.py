e0dc7f99f2fb512d85eabf8671525f8c44c08fda
iamkathan <shethkathan4@gmail.com>
2023-10-04 03:58:57 +0100

    contact page has been added

--- Home/urls.py
+++ Home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
    path('', views.index, name="index"),
    path('service', views.service, name="service"),
    path('about', views.about, name="about"),
+   path('contact', views.contact, name="contact"),
 ]
