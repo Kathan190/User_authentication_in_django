69da4ce0b4aadd25052f037ee39057f88484bcbe
iamkathan <shethkathan4@gmail.com>
2023-10-04 19:45:23 +0100

    message component 50% done

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from Home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -20,5 +21,6 @@ def contact(request):
         comment = request.POST.get("comment")
         contact = Contact(name=name, email=email, phone=phone, comment=comment, date=datetime.today()) 
         contact.save()
+        messages.success(request, "Your message has been sent")
     return render(request, 'contact.html')
     
